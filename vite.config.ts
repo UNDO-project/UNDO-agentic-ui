5ab29464b55cc1b308d83f9080c120d7a9762879
jethronap <efthioa@gmail.com>
2026-05-10 14:52:09 +0300

    Updated stale info pages (#44)

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,14 @@ export default defineConfig({
         target: "http://127.0.0.1:8080",
         changeOrigin: true,
       },
+      "/version": {
+        target: "http://127.0.0.1:8080",
+        changeOrigin: true,
+      },
+      "/health": {
+        target: "http://127.0.0.1:8080",
+        changeOrigin: true,
+      },
     },
   },
 });
